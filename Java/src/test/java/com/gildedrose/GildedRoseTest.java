1c7f41b72e898a47c61f55182401bd0902104b42
Simon <simonrosenberg1@gmail.com>
2021-09-24 13:39:34 +0100

    Refactor: Add test that Sulfuras doesn't degrade in quality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -34,4 +34,14 @@ class GildedRoseTest {
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
     }
+
+    @Test
+    public void sulfurasHandOfRagnarosDoesNotDegrade() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
 }
