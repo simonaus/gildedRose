ac047766fff6b1ab7afa58f44fa674be3daa7fb0
Simon <simonrosenberg1@gmail.com>
2021-09-24 13:35:16 +0100

    Refactor: Add test that item quality does not go below zero

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -24,4 +24,14 @@ class GildedRoseTest {
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
     }
+
+    @Test
+    public void qualityDoesNotGoBelowZero() {
+        Item[] items = new Item[] { new Item("normal", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("normal", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
 }
