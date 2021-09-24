99fafe4a81ea07e364e408491c454485dff84f80
Simon <simonrosenberg1@gmail.com>
2021-09-24 13:33:49 +0100

    Refactor: Add test for normal item after sell date

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
     @Test
-    void normalItemUpdateQualityBeforeSellDate() {
+    public void normalItemUpdateQualityBeforeSellDate() {
         Item[] items = new Item[] { new Item("normal", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("normal", app.items[0].name);
         assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void normalItemUpdateQualityPastSellDate() {
+        Item[] items = new Item[] { new Item("normal", 0, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("normal", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
     }
 }
